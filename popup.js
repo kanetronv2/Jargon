635e90b8f0eae50077160d8f0499218506cfb18e
Kane Hsieh <kane.hsieh@gmail.com>
2016-03-21 20:36:08 -0400

    initial launch

---
+++ popup.js
@@ -0,0 +1,23 @@
+Papa.parse('./dict.csv', {
+  download: true,
+  complete: function(results) {
+
+    console.log(results)
+
+    var len = results.data.length
+    var index = Math.floor((Math.random() * len) + 1);
+
+    setWord(results.data[index][0],results.data[index][1]);
+  }
+});
+
+String.prototype.capitalize = function(){
+    return this.toLowerCase().replace( /\b\w/g, function (m) {
+        return m.toUpperCase();
+    });
+};
+
+function setWord(word, def){
+  document.getElementById('word').innerHTML = word.capitalize();
+  document.getElementById('def').innerHTML = def;
+}
