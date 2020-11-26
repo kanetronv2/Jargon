1ccdaa80575ef39f755baa8c6e1bbf693baff218
Kane Hsieh <uwu@Kanes-MacBook-Pro.local>
2020-11-26 18:27:42 -0500

    dark mode toggle

---
+++ dark.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.btn-toggle');
+
+// Listen for a click on the button
+btn.addEventListener('click', function() {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle('dark-theme');  
+})
