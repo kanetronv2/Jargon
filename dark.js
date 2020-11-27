cc2e1eb86a5d055c6502ee0f65a9ccb3f8b9580b
Kane Hsieh <kane.hsieh@gmail.com>
2020-11-27 00:11:27 -0500

    dark mode toggle

--- dark.js
+++ dark.js
@@ -1,7 +1,20 @@
+// init button
 const btn = document.querySelector('.btn-toggle');
+
+// set state to saved state
+chrome.storage.local.get(['SavedState'], function(result) {
+      document.body.className = result.SavedState;
+    });
 
 // Listen for a click on the button
 btn.addEventListener('click', function() {
   // Then toggle (add/remove) the .dark-theme class to the body
-  document.body.classList.toggle('dark-theme');  
+  document.body.classList.toggle('dark-theme');
+
+  // save state
+  const state = document.body.className;
+  chrome.storage.local.set({'SavedState': state}, function() {
+	  // Notify that we saved.
+	  message('Settings saved');
+	})
 })
